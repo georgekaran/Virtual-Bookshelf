7afd96e8ec97d82a718904746d623dc5a5331301
georgekaran <georgekaran12@gmail.com>
2020-07-12 23:02:53 -0300

    feat: add Container and Grid to HomeView

--- src/pages/Home/HomeView.tsx
+++ src/pages/Home/HomeView.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 export default function Home() {
   return (
-    <div>
-      <Button className="Btn__AddBook">
-        Adicionar livro
-      </Button>
-    </div>
+    <Container fixed 
+               className="Base__Container">
+      <Grid item 
+            xs={12}>
+        <Button className="Btn__AddBook">
+          Adicionar livro
+        </Button>
+      </Grid>
+    </Container>
   )
 }
