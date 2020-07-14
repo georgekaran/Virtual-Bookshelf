877e2f5e2eca2b1d527719a179fc26168021b2f6
georgekaran <georgekaran12@gmail.com>
2020-07-14 19:34:47 -0300

    feat: add Avatar to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Box, Button } from '@material-ui/core';
+import { Box, Button, Divider, Avatar } from '@material-ui/core';
+
+import userAvatar from '../../assets/images/user-avatar.jpg';
 
 export default function Header() {
+  // TODO: Get user info
+
+
   return (
-    <Box className="App__Header" component="header" boxShadow={3}>
-      <Button className="BtnPrimary">Adicionar livro</Button>
-    </Box>
+    <>
+      <Box className="App__Header" 
+           component="header">
+        <Box className="LogoWrapper">
+
+        </Box>
+        <Box className="ActionsWrapper">
+        <Button className="BtnPrimary">
+          Adicionar livro
+        </Button>
+        <Avatar className="Avatar"
+                alt="Awesome user" 
+                src={userAvatar} />
+        </Box>
+      </Box>
+      <Divider />
+    </>
   );
 }
