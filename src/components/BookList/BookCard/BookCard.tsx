9fae6aaedea7efd3a3e909539c3733dbfe2813a5
georgekaran <georgekaran12@gmail.com>
2020-07-13 22:21:37 -0300

    feat: add title and author to BookCard

--- src/components/BookList/BookCard/BookCard.tsx
+++ src/components/BookList/BookCard/BookCard.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import Paper from '@material-ui/core/Paper';
+import { Typography, Paper } from '@material-ui/core';
 
 const BookCard = () => {
   return (
     <Paper className="BookCard" elevation={2}>
       <img src="https://s26162.pcdn.co/wp-content/uploads/2018/12/81A9dFqIEEL.jpg" alt="Book Cover" />
+      <Typography className="Title" 
+                  component="label">
+        Lorem ipsum dolor        
+      </Typography>
+      <Typography className="Author" 
+                  component="label">
+        Lorem ipsum dolor        
+      </Typography>
     </Paper>
   )
 }
