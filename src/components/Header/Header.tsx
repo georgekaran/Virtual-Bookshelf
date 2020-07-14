e5f83a1b77fca26ab2589a63864b234f2499b575
georgekaran <georgekaran12@gmail.com>
2020-07-14 19:43:17 -0300

    feat: add sticky position to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,24 +6,26 @@ import userAvatar from '../../assets/images/user-avatar.jpg';
 export default function Header() {
   // TODO: Get user info
 
-
   return (
     <>
-      <Box className="App__Header" 
-           component="header">
-        <Box className="LogoWrapper">
+      <Box className="App__Header" component="header">
+        <Box display="flex" 
+             justifyContent={'space-between'} 
+             height="100%">
+          <Box className="LogoWrapper">
 
+          </Box>
+          <Box className="ActionsWrapper">
+            <Button className="BtnPrimary">
+              Adicionar livro
+            </Button>
+            <Avatar className="Avatar" 
+                    alt="Awesome user" 
+                    src={userAvatar} />
+          </Box>
         </Box>
-        <Box className="ActionsWrapper">
-        <Button className="BtnPrimary">
-          Adicionar livro
-        </Button>
-        <Avatar className="Avatar"
-                alt="Awesome user" 
-                src={userAvatar} />
-        </Box>
+        <Divider />
       </Box>
-      <Divider />
     </>
   );
 }
