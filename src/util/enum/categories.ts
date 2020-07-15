a9ea7bd776f7ff703d68abf0e0b1ff9cff72e8a5
georgekaran <georgekaran12@gmail.com>
2020-07-14 21:51:34 -0300

    feat: create categories enum and protocols folder

---
+++ src/util/enum/categories.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../protocols'
+
+const categories: Category[] = [{
+  name: 'reading',
+  title: 'Lendo'
+}, {
+  name: 'wantToRead',
+  title: 'Quero ler'
+}, {
+  name: 'read',
+  title: 'Lidos'
+}]
+
+export default categories
