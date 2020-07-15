1a49b22552eaca31a0e422fb1f9bb4c834e0b13e
georgekaran <georgekaran12@gmail.com>
2020-07-15 20:41:17 -0300

    feat: redirect book click to BookView

--- src/components/BookList/BookCard/BookCard.tsx
+++ src/components/BookList/BookCard/BookCard.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Typography, Paper } from '@material-ui/core';
 
 const BookCard = () => {
+  const history = useHistory();
+
+  const handleRedirectBookView = (bookId: number) => {
+    history.push(`/book/${bookId}`);
+  }
+
   return (
-    <Paper className="BookCard" elevation={2}>
+    <Paper className="BookCard" 
+           elevation={2} 
+           onClick={() => handleRedirectBookView(1)}>
       <img src="https://s26162.pcdn.co/wp-content/uploads/2018/12/81A9dFqIEEL.jpg" alt="Book Cover" />
       <div className="InfoWrapper">
         <Typography className="Title" component="label">
