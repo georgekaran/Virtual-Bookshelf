f1733760c9694e442eff2a223b456142d5f1ce98
georgekaran <georgekaran12@gmail.com>
2020-07-14 22:15:19 -0300

    feat: create BookView and FormBook

---
+++ src/pages/BookView/View/BookView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BookView() {
+  return (
+    <div>
+      View Book
+    </div>
+  )
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -8,6 +8,8 @@ import {
 import Home from '../pages/Home/HomeView'
 import Category from '../pages/Category/CategoryView'
 import Header from '../components/Header/Header'
+import BookView from '../pages/BookView/View/BookView'
+import FormBook from '../pages/BookView/Form/FormBook'
 
 export default function Router() {
   return (
@@ -19,6 +21,12 @@ export default function Router() {
           </Route>
           <Route path="/category/:categoryName">
             <Category />
+          </Route>
+          <Route path="/book/:id">
+            <BookView />
+          </Route>
+          <Route path="/book">
+            <FormBook />
           </Route>
         </Switch>
     </BrowserRouter>
