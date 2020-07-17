787c1203500c1c00658873bed0e4d632f0e34c67
georgekaran <georgekaran12@gmail.com>
2020-07-16 21:57:23 -0300

    feat: init FormBook component

--- src/pages/BookView/Form/FormBook.tsx
+++ src/pages/BookView/Form/FormBook.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Grid, Box, Typography } from '@material-ui/core'
+
+import Dropzone from '../../../components/Dropzone/Dropzone';
 
 export default function FormBook() {
+  const [image, setImage] = useState<File>();
+
   return (
-    <div>
-      Form Book
-    </div>
+    <Container fixed className={`Form__Container`}>
+      <Grid item xs={12}>
+        <Box className="Box">
+          <Typography variant="h4" component="h4">
+            Cadastro de livro
+          </Typography>
+          <Dropzone onFileUploaded={setImage} />
+        </Box>
+      </Grid>
+    </Container>
   )
 }
