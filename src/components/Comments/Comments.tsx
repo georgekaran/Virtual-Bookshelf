7d33f2560406b694b81fc1f953f265a771461ed2
georgekaran <georgekaran12@gmail.com>
2020-07-21 21:13:34 -0300

    feat: sort comments by date

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Box, Typography } from '@material-ui/core';
 
@@ -6,12 +6,16 @@ import Comment from '../Comment/Comment';
 import Api from '../../util/api/api';
 import { setComments } from '../../actions/commentsActions';
 import { RootState } from '../../protocols/root-state';
+import { CommentModel } from '../../protocols';
+import sortDateDesc from '../../sort/sortDateDesc';
 
 interface CommentsProps {
   bookId: string
 }
 
 const Comments: React.FC<CommentsProps> = ({ bookId }) => {
+  const [commentsSorted, setcommentsSorted] = useState<CommentModel[]>([])
+
   const dispatch = useDispatch();
   const comments = useSelector((state: RootState) => state.comments);
 
@@ -23,10 +27,16 @@ const Comments: React.FC<CommentsProps> = ({ bookId }) => {
 
   useEffect(fetchComments, [bookId])
 
+  const sortCommentsByDate = () => {
+    setcommentsSorted(sortDateDesc<CommentModel>("timestamp", comments))
+  }
+
+  useEffect(sortCommentsByDate, [comments])
+
   return (
     <Box className="CommentsWrapper">
-      <Typography className="Title">Comments({comments.length})</Typography>
-      {comments.map(comment => (
+      <Typography className="Title">Comments({commentsSorted.length})</Typography>
+      {commentsSorted.map(comment => (
         <Comment key={comment.id} 
                  comment={comment} />
       ))}
